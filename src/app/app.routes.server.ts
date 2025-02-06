8354205c47bc1d8482d51725a82183170e60d5f0
Tilak Varma <tv35302@YSIDEV.YARDI.CLOUD>
2025-02-06 11:31:19 +0530

    Changes made for some reason

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,13 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path:'',
+    renderMode:RenderMode.Server
+  },
+  {
+    path:"/subtask/:id",
+    renderMode:RenderMode.Server
   }
 ];
